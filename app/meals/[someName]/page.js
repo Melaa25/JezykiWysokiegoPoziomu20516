42a4953e0de67ba56a170a86cb828f067421cfd3
Emilia <20516@student.ans-elblag.pl>
2025-01-25 18:16:23 +0100

    test13

--- app/meals/[someName]/page.js
+++ app/meals/[someName]/page.js
@@ -1,7 +1,42 @@
 
+import { getMeal } from '@/lib/meals.js';
+import classes from './page.module.css';
 
 export default async function MealDetailsPage({ params }) {
-  const { someName } =  await params;
+  const { someName } =await  params;
+  const meal = getMeal(someName);
 
-  return <h1>Meal Details for {someName}</h1>;
+  if (!meal) {
+    return (
+      <main>
+        <div className={classes.loading}>Loading meal details...</div>
+      </main>
+    );
+  }
+
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Discover the secret of{' '}
+          <span className={classes.highlight}>{meal.name}</span>
+        </h1>
+        <p>
+          Created by <a href="#" className={classes.link}>{meal.creator}</a>
+        </p>
+        <p>
+          {meal.summary}
+        </p>
+      </header>
+      <main className={classes.main}>
+        <div className={classes.image}>
+          <img src={meal.image} alt={meal.name} />
+        </div>
+        <section className={classes.instructions}>
+          <h2>How to Prepare</h2>
+          <p>{meal.instructions}</p>
+        </section>
+      </main>
+    </>
+  );
 }
