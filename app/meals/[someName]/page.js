4c499c9c5894ca6330008ab670a3f89f04c893ea
Emilia <20516@student.ans-elblag.pl>
2025-01-25 18:42:30 +0100

    t

--- app/meals/[someName]/page.js
+++ app/meals/[someName]/page.js
@@ -1,42 +1,45 @@
-
-import { getMeal } from '@/lib/meals.js';
 import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
 
-export default async function MealDetailsPage({ params }) {
-  const { someName } =await  params;
+export default async function MealDetailsPage(context) {
+  // Odczytujemy dynamiczny parametr
+  const { params } = context;
+  const someName = params;
+
+  // Pobieranie szczegółów dania z bazy danych
   const meal = getMeal(someName);
 
+  // Obsługa przypadku, gdy danie nie istnieje
   if (!meal) {
     return (
-      <main>
-        <div className={classes.loading}>Loading meal details...</div>
+      <main className={classes.main}>
+        <p className={classes.loading}>Meal not found...</p>
       </main>
     );
   }
 
+  // Renderowanie szczegółów dania
   return (
-    <>
-      <header className={classes.header}>
-        <h1>
-          Discover the secret of{' '}
-          <span className={classes.highlight}>{meal.name}</span>
-        </h1>
-        <p>
-          Created by <a href="#" className={classes.link}>{meal.creator}</a>
-        </p>
-        <p>
-          {meal.summary}
-        </p>
-      </header>
-      <main className={classes.main}>
+    <main className={classes.main}>
+      <header className={`${classes.header} ${classes.reversedHeader}`}>
+        <div className={classes.headerText}>
+          <h1 className={`${classes.title} ${classes.highlight}`}>{meal.name}</h1>
+          <p className={classes.summary}>{meal.summary}</p>
+          <div className={classes.creator}>
+            <span>Created by </span>
+            <a href="#" className={classes.link}>
+              {meal.creator}
+            </a>
+          </div>
+        </div>
         <div className={classes.image}>
           <img src={meal.image} alt={meal.name} />
         </div>
-        <section className={classes.instructions}>
-          <h2>How to Prepare</h2>
-          <p>{meal.instructions}</p>
-        </section>
-      </main>
-    </>
+      </header>
+      <section className={classes.instructions}>
+        <h2>How to Prepare</h2>
+        <p>{meal.instructions}</p>
+      </section>
+    </main>
   );
 }
