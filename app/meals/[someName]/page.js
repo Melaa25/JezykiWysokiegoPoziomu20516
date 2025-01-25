eb290ad8f5db377e9468b5e6fbe905f4b168d228
Emilia <20516@student.ans-elblag.pl>
2025-01-25 18:44:16 +0100

    te

--- app/meals/[someName]/page.js
+++ app/meals/[someName]/page.js
@@ -1,45 +1,7 @@
-import classes from './page.module.css';
-import { getMeal } from '@/lib/meals';
 
-export default async function MealDetailsPage(context) {
-  // Odczytujemy dynamiczny parametr
-  const { params } = context;
-  const someName = params;
 
-  // Pobieranie szczegółów dania z bazy danych
-  const meal = getMeal(someName);
+export default async function MealDetailsPage({ params }) {
+  const { someName } =  await params;
 
-  // Obsługa przypadku, gdy danie nie istnieje
-  if (!meal) {
-    return (
-      <main className={classes.main}>
-        <p className={classes.loading}>Meal not found...</p>
-      </main>
-    );
-  }
-
-  // Renderowanie szczegółów dania
-  return (
-    <main className={classes.main}>
-      <header className={`${classes.header} ${classes.reversedHeader}`}>
-        <div className={classes.headerText}>
-          <h1 className={`${classes.title} ${classes.highlight}`}>{meal.name}</h1>
-          <p className={classes.summary}>{meal.summary}</p>
-          <div className={classes.creator}>
-            <span>Created by </span>
-            <a href="#" className={classes.link}>
-              {meal.creator}
-            </a>
-          </div>
-        </div>
-        <div className={classes.image}>
-          <img src={meal.image} alt={meal.name} />
-        </div>
-      </header>
-      <section className={classes.instructions}>
-        <h2>How to Prepare</h2>
-        <p>{meal.instructions}</p>
-      </section>
-    </main>
-  );
+  return <h1>Meal Details for {someName}</h1>;
 }
